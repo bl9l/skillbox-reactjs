f724b1f54d2f266050cdb6a8dcaa6083314405d0
Bogdan Rudakov <bogdan@desire-s.ru>
2021-10-24 16:28:46 +0500

    feat: 2.4 Подключаем typescript
    - настроил tsconfig.json
    - создал тестовый компонент Example.tsx
    
    Итог: работает typescript
    HMR работает 50/50, выдаёт ошибку `process-update.js:126 [HMR] Update check failed: TypeError: Failed to fetch`

---
+++ src/shared/Example.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {hot} from "react-hot-loader/root";
+
+const ExampleComponent = () => <div>
+  Typesctipt works qwe
+</div>
+
+export const Example = hot(ExampleComponent);
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,8 +1,10 @@
 import {hot} from 'react-hot-loader/root'
 import * as React from "react";
+import {Example} from "./Example";
 
 function HeaderComponent() {
     return (<header>
+        <Example/>
         <h1>Reddit for our own))) _</h1>
     </header>);
 }
