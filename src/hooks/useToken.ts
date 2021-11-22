8ab82cd0c11a1cf1355d112f855168862f6c9b9f
Bogdan Rudakov <bogdan@desire-s.ru>
2021-11-22 21:42:10 +0500

    error: hooks

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -2,6 +2,8 @@ import {useEffect, useState} from "react";
 
 export function useToken() {
   const [token, setToken] = useState('');
+
+  // Компилятор ругается на неверный выхов хука
   useEffect(() => {
     if (window.__token__) {
       setToken(window.__token__);
