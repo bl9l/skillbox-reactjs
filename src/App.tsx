ab735f2a33bd47d370c4637096286016155411e5
Bogdan Rudakov <bogdan@desire-s.ru>
2021-11-29 20:06:53 +0500

    10.2 Устанавливаем Redux, подключаем его к приложению

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,31 @@ import {useToken} from "./hooks/useToken";
 import {tokenContext} from "./contexts/tokenContext";
 import {UserContextProvider} from "./contexts/userContext";
 import {PostsContextProvider} from "./contexts/postsContext";
+import {createStore} from "redux";
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from "redux-devtools-extension";
+
+const store = createStore(() => {}, composeWithDevTools());
 
 function AppComponent() {
   const [token] = useToken();
 
-  return <tokenContext.Provider value={token}>
-    <UserContextProvider>
-      <Layout>
-        <Header/>
-        <Content>
-          <PostsContextProvider>
-            <CardsList/>
-          </PostsContextProvider>
-        </Content>
-      </Layout>
-    </UserContextProvider>
-  </tokenContext.Provider>
+  return (
+    <Provider store={store}>
+      <tokenContext.Provider value={token}>
+        <UserContextProvider>
+          <Layout>
+            <Header/>
+            <Content>
+              <PostsContextProvider>
+                <CardsList/>
+              </PostsContextProvider>
+            </Content>
+          </Layout>
+        </UserContextProvider>
+      </tokenContext.Provider>
+    </Provider>
+  )
 }
 
 export const App = hot(() => <AppComponent/>);
