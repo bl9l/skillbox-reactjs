603aaef638f9fda65809953e0f33ebbd7b1dec78
Bogdan Rudakov <bogdan@desire-s.ru>
2021-11-29 20:44:51 +0500

    10.4 Presentation Components/Container Components

--- src/shared/Post/PostCommentForm/PostCommentForm.tsx
+++ src/shared/Post/PostCommentForm/PostCommentForm.tsx
@@ -1,36 +1,24 @@
 import React, {ChangeEvent, FormEvent, useEffect, useRef} from 'react';
 import styles from './postcommentform.scss';
-import {useDispatch, useSelector} from "react-redux";
-import {RootState, updateComment} from "../../../store";
 
 interface IPostCommentFormProps {
-  defaultReplyText?: string;
+  value?: string;
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void,
+  onSubmit: (e: FormEvent) => void
 }
 
-export function PostCommentForm({defaultReplyText = ''}: IPostCommentFormProps) {
-  const value = useSelector<RootState, string>(state => state.commentText);
-  const dispatch = useDispatch();
+export function PostCommentForm({value = '', onChange, onSubmit}: IPostCommentFormProps) {
 
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     inputRef.current?.focus();
     // переводим каретку в конец строки
-    inputRef.current?.setSelectionRange(defaultReplyText?.length || 0, defaultReplyText?.length || 0);
+    inputRef.current?.setSelectionRange(value?.length || 0, value?.length || 0);
   }, []);
 
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-
-    console.log(value);
-  }
-
-  function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
-    dispatch(updateComment(e.target.value));
-  }
-
-  return (<form className={styles.form} onSubmit={handleSubmit}>
-    <textarea className={styles.input} value={value} onChange={handleChange} ref={inputRef}/>
+  return (<form className={styles.form} onSubmit={onSubmit}>
+    <textarea className={styles.input} value={value} onChange={onChange} ref={inputRef}/>
     <button type='submit' className={styles.button}>Комментировать</button>
   </form>);
 }
