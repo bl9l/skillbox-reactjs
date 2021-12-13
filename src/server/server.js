02d6d4007035b661d7f47cb396f6d9829d7f130e
Bogdan Rudakov <bogdan@desire-s.ru>
2021-12-13 18:10:05 +0500

    fix env

--- src/server/server.js
+++ src/server/server.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/server';
 import {indexTemplate} from './indexTemplate';
 import {App} from "../App";
 import axios from 'axios';
+import compression from 'compression';
+import helmet from 'helmet';
 
 import jsdom from 'jsdom';
 global.document = (new jsdom.JSDOM).window.document;
 
 const PORT = process.env.PORT || 3000;
+const IS_DEV = process.env.NODE_ENV  !== 'production';
 
 const app = express();
+
+if (!IS_DEV) {
+    app.use(compression());
+    app.use(helmet({
+        contentSecurityPolicy: false,
+    }));
+}
 
 app.use('/static', express.static('./dist/client'));
 
